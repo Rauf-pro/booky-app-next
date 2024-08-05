7ec81c62e4765b6e92916d7e0a9b5286710ae482
rrauf8178 <rrauf8178@gmail.com>
2024-08-05 21:59:07 +0400

    add: dynamic room images to RoomList component

--- frontend/app/components/RoomList.tsx
+++ frontend/app/components/RoomList.tsx
@@ -5,10 +5,18 @@ import Link from "next/link";
 import Image from "next/image";
 
 const RoomList = ({ rooms }: { rooms: any }) => {
+    
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {rooms.data.map((room: any) => {
-        return <div key={room.id}>room</div>;
+        const imgUrl = `http://127.0.0.1:1337${room.attributes.image.data?.attributes.url}`;
+        return <div key={room.id}>
+            <Link href={`/room/${room.id}`}>
+              <div className="relative w-full h-[300px] overflow-hidden mb-6">
+                <Image src={imgUrl} fill priority alt="" className="object-cover"/>
+              </div>
+            </Link>
+        </div>;
       })}
     </div>
   );
