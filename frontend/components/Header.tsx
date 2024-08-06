ac02b2ad755f03fd546238f312456a7ba88f2d4e
rrauf8178 <rrauf8178@gmail.com>
2024-08-06 18:42:20 +0400

    add: using kinde auth add login and register butons and chadcdn button as dependencies

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,13 +1,69 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FaYoutube, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
+const socials = [
+  { icon: <FaYoutube />, href: "https://www.youtube.com" },
+  { icon: <FaFacebook />, href: "https://www.facebook.com" },
+  { icon: <FaInstagram />, href: "https://www.instagram.com" },
+  { icon: <FaTwitter />, href: "https://www.twitter.com" },
+];
 
-const Header = () => {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
+const Header = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  console.log(isUserAuthenticated);
   return (
-    <header>
-      <div className="container mx-auto bg-yellow-300">
-         header
+    <header className="py-6 shadow-md">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row md:justify-between gap-6">
+          {/* logo & social icons */}
+          <div className="flex items-center gap-5 justify-center xl:w-max">
+            {/* logo */}
+            <Link href={"/"}>
+              <Image src={"/assets/logo.svg"} width={160} height={160} alt="" />
+            </Link>
+            {/* separator */}
+            <div className="w-[1px] h-[40px] bg-gray-300"></div>
+            {/* social icons */}
+            <div className="flex gap-2">
+              {socials.map((social, index) => {
+                return (
+                  <Link
+                    href={social.href}
+                    key={index}
+                    className="bg-accent text-white hover:bg-accent-hover text-sm 
+                    w-[28px] h-[28px] flex items-center justify-center rounded-full transition-all"
+                  >
+                    {social.icon}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          {/* sign in & sign up btns */}
+          <div>
+            <div>
+              {isUserAuthenticated ? (
+                <div>dropdown</div>
+              ) : (
+                <div className="flex gap-2">
+                  <LoginLink>Sign in</LoginLink>
+                  <RegisterLink>Sign up</RegisterLink>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
